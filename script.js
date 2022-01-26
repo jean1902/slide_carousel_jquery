6a5f6f5c8073b4895b7431a67d5ce8fc90394248
jean1902 <cin71781@gmail.com>
2022-01-26 10:31:46 +0100

    premier commit

---
+++ script.js
@@ -0,0 +1,15 @@
+
+      
+//slider multiple
+$('.Slider').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+  });
+          
+  // let prev= document.querySelector('.prev').innerHTML =`<i class=" fas fa-arrow-circle-left" ></i>`
+  // let next= document.querySelector('.next').innerHTML=`<i class=" fas fa-arrow-circle-right" ></i>`
+  
+       
+  
